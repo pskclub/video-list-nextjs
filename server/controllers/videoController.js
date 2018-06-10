5982111eb32098006f11ad92e64fd00e36fddc83
Passakon Puttasuwan <passakon@getlinks.co>
2018-06-10 22:56:40 +0700

    Fix error msg

--- server/controllers/videoController.js
+++ server/controllers/videoController.js
@@ -3,8 +3,10 @@ const axios = require('axios')
 exports.getVideos = async (req, res) => {
   try {
     const result = await axios.get('https://s3-ap-southeast-1.amazonaws.com/ysetter/media/video-search.json')
-    res.json(result.data)
+    res.status(200).json(result.data)
   } catch (e) {
-    res.json(e)
+    res.status(500).json({
+      code : 'INTERNAL_SERVER_ERROR'
+    })
   }
 }
